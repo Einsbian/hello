bfa024f4ca4fc775a84288d607f2eb975f441abe
wangke <wangk@inedcn.com>
2019-02-11 16:14:59 +0800

    debug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { Capacitor } from '@capacitor/core';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -22,5 +25,22 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+    
+    const isAvailable = Capacitor.isPluginAvailable('Camera');
+
+    if (!isAvailable) {
+      // Have the user upload a file instead
+    } else {
+      // Otherwise, make the call:
+      // Camera.getPhoto()
+    }
+    // if ('serviceWorker' in navigator) {
+    //   console.log(11111111111)
+    //   // Use the window load event to keep the page load performant
+    //   window.addEventListener('load', () => {
+    //     console.log('22222222222222222')
+    //     navigator.serviceWorker.register('./sw.js');
+    //   });
+    // }
   }
 }
